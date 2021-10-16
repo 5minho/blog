24c459e46821893608412a5a75673895a8f634fb
5minho <5minhominho@gmail.com>
2021-10-17 01:20:04 +0900

    first commit

---
+++ api-limit/src/main/java/me/minho/apilimit/ApiLimitApplication.java
@@ -0,0 +1,13 @@
+package me.minho.apilimit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLimitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiLimitApplication.class, args);
+    }
+
+}
---
+++ api-limit/src/test/java/me/minho/apilimit/ApiLimitApplicationTests.java
@@ -0,0 +1,13 @@
+package me.minho.apilimit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLimitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
